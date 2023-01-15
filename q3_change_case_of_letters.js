e40aa427597b8fe5881bcfd46ac5018e7f6eed70
Mehak Noor Singh <mehaknoor2k2@gmail.com>
2023-01-15 15:37:43 +0530

    feat: change case of every character in string

---
+++ q3_change_case_of_letters.js
@@ -0,0 +1,26 @@
+
+function changeCaseByFunction(str) {
+    let answer = ""
+    for (let index = 0; index < str.length; index++) {
+        if (str[index] >= 'a' && str[index] <= 'z')
+            answer += str[index].toUpperCase();
+        else answer += str[index].toLowerCase();
+    }
+
+    return answer
+}
+
+const changeCaseByArrow = (str) => {
+
+    let answer = ""
+    for (let index = 0; index < str.length; index++) {
+        if (str[index] >= 'a' && str[index] <= 'z')
+            answer += str[index].toUpperCase();
+        else answer += str[index].toLowerCase();
+    }
+
+    return answer
+
+}
+
+module.exports = { changeCaseByFunction, changeCaseByArrow }
---
+++ tests/q3_change_case_of_letters.test.js
@@ -0,0 +1,30 @@
+
+const { changeCaseByFunction, changeCaseByArrow } = require('../q3_change_case_of_letters');
+
+
+describe('General Statements', () => {
+
+    test('NORMAL FUNCTION', () =>{
+        expect(changeCaseByFunction('MeRRy hAD a LITTle lAMp')).toEqual('mErrY Had A littLE LamP')
+        expect(changeCaseByFunction('Hello World')).toEqual('hELLO wORLD')
+    })
+    
+    test('ARROW FUNCTION', () =>{
+        expect(changeCaseByArrow('MeRRy hAD a LITTle lAMp')).toEqual('mErrY Had A littLE LamP')
+        expect(changeCaseByArrow('Hello World')).toEqual('hELLO wORLD')
+    })
+})
+
+describe('Strings with special characters', () => {
+
+    test('NORMAL FUNCTION', () =>{
+        expect(changeCaseByFunction('MeRRy @hAD a LITTle lAMp')).toEqual('mErrY @Had A littLE LamP')
+        expect(changeCaseByFunction('Hello Worl!123d')).toEqual('hELLO wORL!123D')
+    })
+    
+    test('ARROW FUNCTION', () =>{
+        expect(changeCaseByArrow('MeRRy @hAD a LITTle lAMp')).toEqual('mErrY @Had A littLE LamP')
+        expect(changeCaseByArrow('Hello Worl!123d')).toEqual('hELLO wORL!123D')
+    })
+})
+
