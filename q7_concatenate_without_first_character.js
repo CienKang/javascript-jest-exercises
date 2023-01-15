0ecf95b618ef562e97bde78ab2cb37bfb8aa13c5
Mehak Noor Singh <mehaknoor2k2@gmail.com>
2023-01-15 17:48:25 +0530

    feat: concatenate two strings omitting their first characters

---
+++ q7_concatenate_without_first_character.js
@@ -0,0 +1,24 @@
+
+function concatenateWithoutFirstCharacterFunction(arr) {
+
+    if (arr.length != 2)
+        return "-1"
+
+    let concatenatedString = ""
+    for (let index = 0; index < arr.length; index++)
+        concatenatedString += arr[index].slice(1, arr[index].length)
+
+    return concatenatedString
+}
+
+const concatenateWithoutFirstCharacterArrow = (arr) => {
+
+    if (arr.length != 2)
+        return "-1"
+
+    return arr.reduce((finalStr, currStr) =>
+        finalStr = finalStr + currStr.slice(1, currStr.length)
+        , "")
+}
+
+module.exports = { concatenateWithoutFirstCharacterFunction, concatenateWithoutFirstCharacterArrow }
