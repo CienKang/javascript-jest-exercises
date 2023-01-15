6819ff61a0e6c3b02e0976e68f8db2cab3c24ba7
Mehak Noor Singh <mehaknoor2k2@gmail.com>
2023-01-15 18:08:15 +0530

    feat: shift alphabets in string by one

---
+++ tests/q9_shift_alphabet_by_one.test.js
@@ -0,0 +1,14 @@
+const { shiftAlphabetByOneFunction, shiftAlphabetByOneArrow } = require('../q9_shift_alphabet_by_one')
+
+describe('General cases', () => {
+
+    test('NORMAL FUNCTION', () => {
+        expect(shiftAlphabetByOneFunction("Codeacademy")).toBe("Dpefbdbefnz")
+        expect(shiftAlphabetByOneFunction("laZyinterns")).toBe("mbAzjoufsot")
+    })
+
+    test('ARROW FUNCTION', () => {
+        expect(shiftAlphabetByOneArrow("Codeacademy")).toBe("Dpefbdbefnz")
+        expect(shiftAlphabetByOneArrow("laZyinterns")).toBe("mbAzjoufsot")
+    })
+})
