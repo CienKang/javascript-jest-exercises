10bf0c65d863bc2f5b1154d4a598504c19f0a025
Mehak Noor Singh <mehaknoor2k2@gmail.com>
2023-01-16 20:26:42 +0530

    feat: add and test triple the number and filter even by map,filter and single reduce

--- arrayUtils.js
+++ arrayUtils.js
@@ -4,6 +4,32 @@ const doubleNumbers = (arr) => {
     if (Array.isArray(arr) == false)
         throw new Error(TypeError);
 
+    arr.forEach(element => {
+        if (typeof (element) !== 'number')
+            throw new Error('Array contains element other than number');
+    });
+
     return arr.map(x => x * 2);
 };
-module.exports = { doubleNumbers };
+
+const tripleNumberAndFilterEven = (arr) => {
+
+    if (Array.isArray(arr) == false)
+        throw new Error(TypeError);
+
+    arr.forEach(element => {
+        if (typeof (element) !== 'number')
+            throw new Error('Array contains element other than number');
+    });
+
+    // return arr.map(x => x * 3).filter(x => (x % 2 == 0));
+
+    return arr.reduce((newArr, element) => {
+        if ((element * 3) % 2 === 0)
+            return [...newArr,3*element];
+        return newArr;
+    }, []);
+};
+
+console.log(tripleNumberAndFilterEven([1, 2, 3, 4]));
+module.exports = { doubleNumbers, tripleNumberAndFilterEven };
--- tests/arrayUtilis.test.js
+++ tests/arrayUtilis.test.js
@@ -1,17 +1,36 @@
 /*eslint-disable*/
-const { doubleNumbers } = require('../arrayUtils');
+const { doubleNumbers, tripleNumberAndFilterEven } = require('../arrayUtils');
 
 
 describe('Array Utilis', () => {
 
     describe('Double Numbers', () => {
 
-        it('should return -1 when input is not an array', () => {
+        it('should return Error when input is not an array', () => {
             expect(() => doubleNumbers('absc')).toThrow(Error(TypeError));
         });
 
-        it('should return an array with doubled element when input is an array',()=>{
-            expect(doubleNumbers([1,2,3])).toEqual([2,4,6]);
+        it('should return Error when input is not an array', () => {
+            expect(() => doubleNumbers([3, 'absc'])).toThrow(Error('Array contains element other than number'));
+        });
+
+        it('should return an array with doubled element when input is an array', () => {
+            expect(doubleNumbers([1, 2, 3])).toEqual([2, 4, 6]);
+        });
+    });
+
+    describe('Triple Number And Filter Even', () => {
+
+        it('should return Error when input is not an array', () => {
+            expect(() => tripleNumberAndFilterEven('absc')).toThrow(Error(TypeError));
+        });
+
+        it('should return Error when input is not an array', () => {
+            expect(() => tripleNumberAndFilterEven([3, 'absc'])).toThrow(Error('Array contains element other than number'));
+        });
+
+        it('should return an array with doubled element when input is an array', () => {
+            expect(tripleNumberAndFilterEven([1, 2, 3])).toEqual([6]);
         });
     });
 });
